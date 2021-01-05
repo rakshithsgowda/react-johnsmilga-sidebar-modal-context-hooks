03d57b77fb505c085a43120bf5d6c463fe8a7689
rakshith <rakshit.s.gowda@gmail.com>
2021-01-05 13:02:43 +0530

    first commit unfinished content

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { FaBars } from 'react-icons/fa';
+import { AppContext } from './context'
+
+const Home = () => {
+  const data = useContext(AppContext)
+  return (
+    <main>
+      <button className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button className="btn">show modal</button>
+    </main>
+  )
+}
+
+export default Home
